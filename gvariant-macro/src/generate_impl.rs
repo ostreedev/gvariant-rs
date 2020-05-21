24379d97e1bb5aa39f419550320ae262d19adbad
William Manley <will@stb-tester.com>
2020-05-21 17:44:52 +0100

    Macro generalisation: Generalise alignment

--- gvariant-macro/src/generate_impl.rs
+++ gvariant-macro/src/generate_impl.rs
@@ -1,11 +1,13 @@
-use crate::type_parser::one;
 use std::error::Error;
 use std::io::Write;
+
+use crate::type_parser::{one, GVariantType};
 
 pub(crate) fn generate_types(gv_typestr: &[u8]) -> Result<String, Box<dyn Error>> {
     let spec = one(gv_typestr)?;
     let mut code: Vec<u8> = vec![];
     let name = "Structure".to_owned() + escape(spec.to_string()).as_ref();
+    let alignment = align_of(&spec);
     write!(code, "
 mod _gvariant_macro {{
     #[macro_use]
@@ -16,10 +18,10 @@ mod _gvariant_macro {{
     #[derive(Debug, RefCast)]
     #[repr(transparent)]
     pub(crate) struct {name} {{
-        data: ::gvariant::aligned_bytes::AlignedSlice<::gvariant::aligned_bytes::A4>,
+        data: ::gvariant::aligned_bytes::AlignedSlice<::gvariant::aligned_bytes::A{alignment}>,
     }}
     impl GVariantMarker for {name} {{
-        type Alignment = ::gvariant::aligned_bytes::A4;
+        type Alignment = ::gvariant::aligned_bytes::A{alignment};
         const SIZE: Option<usize> = None;
         fn _mark(data: &::gvariant::aligned_bytes::AlignedSlice<Self::Alignment>) -> &Self {{
             Self::ref_cast(data.as_ref())
@@ -45,7 +47,7 @@ mod _gvariant_macro {{
         }}
     }}
 }}
-", name=name)?;
+", name=name, alignment=alignment)?;
     Ok(String::from_utf8(code)?)
 }
 
@@ -61,4 +63,45 @@ pub(crate) fn escape(x: String) -> String {
         }
     }
     String::from_utf8(out).unwrap()
+}
+
+fn align_of(t: &GVariantType) -> usize {
+    match t {
+        GVariantType::B | GVariantType::Y => 1,
+        // Each integer type has alignment equal to its fixed size.
+        GVariantType::N | GVariantType::Q => 2,
+        GVariantType::I | GVariantType::U => 4,
+        GVariantType::X | GVariantType::T => 8,
+        GVariantType::D => 8,
+        // Including object paths and signature strings, strings are not
+        // fixed-sized and have an alignment of 1.
+        GVariantType::S | GVariantType::O | GVariantType::G => 1,
+        // The alignment of a maybe type is always equal to the alignment of
+        // its element type.
+        GVariantType::M(t) => align_of(t),
+        // The alignment of an array type is always equal to the alignment of
+        // its element type.
+        GVariantType::A(t) => align_of(t),
+        // The alignment of a container type is equal to the largest alignment
+        // of any potential child of that container.
+        GVariantType::Tuple(subtypes) => subtypes.iter().map(align_of).max().unwrap_or(1),
+        GVariantType::DictItem(subtypes) => align_of(&subtypes[0]).max(align_of(&subtypes[1])),
+        // This means that the variant type has an alignment of 8 (since it
+        // could potentially contain a value of any other type and the maximum
+        // alignment is 8).
+        GVariantType::V => 8,
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_alignment() {
+        assert_eq!(align_of(&one(b"s").unwrap()), 1);
+        assert_eq!(align_of(&one(b"i").unwrap()), 4);
+        assert_eq!(align_of(&one(b"(uy)").unwrap()), 4);
+        assert_eq!(align_of(&one(b"(ti)").unwrap()), 8);
+    }
 }
