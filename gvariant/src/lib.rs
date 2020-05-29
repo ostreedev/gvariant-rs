e096eef3098a2a367b9515864a639f8d74430542
William Manley <will@stb-tester.com>
2020-05-29 11:05:50 +0100

    Update docs to match README and current status

--- gvariant/src/lib.rs
+++ gvariant/src/lib.rs
@@ -39,14 +39,12 @@
 //!
 //! ## Status
 //!
-//! * Support for all GVariant types is implemented apart from dictionary
-//!   entries.
+//! * Support for all GVariant types is implemented
 //! * Serialisation is not currently supported, but may be implemented in a
 //!   future version, or possibly as a seperate crate.
 //!
 //! ### TODO
 //!
-//! * Implement support for dict items
 //! * Correct handling of non-normal structs
 //! * Add no-std and no-alloc support
 //! * Fuzz testing - compare against the GLib version
